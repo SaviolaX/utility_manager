faa9793a1da37432cb90edc9a306204b108529b3
SaviolaX <withcraft1717@gmail.com>
2025-03-27 13:58:42 +0200

    infra: Add outputs for UserPool and UserPool client

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -16,5 +16,14 @@ output "bucket_arn" {
 output "website_endpoint" {
   value       = aws_s3_bucket_website_configuration.frontend_bucket_website_configuration.website_endpoint
   description = "The URL of the deployed application"
+}
 
+output "cognito_user_pool_id" {
+  value       = aws_cognito_user_pool.utility_manager_user_pool.id
+  description = "The Cognito UserPool ID"
+}
+
+output "cognito_user_pool_client_id" {
+  value       = aws_cognito_user_pool_client.utility_manager_user_pool_client.id
+  description = "The Cognito UserPool client ID"
 }
