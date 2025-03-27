ed0cbd56c470b009d3cd5eea224b76fef7f754d3
SaviolaX <withcraft1717@gmail.com>
2025-03-27 13:57:38 +0200

    fix: Change method authorization

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_method" "utility_manager_api_gateway_method" {
   rest_api_id      = aws_api_gateway_rest_api.utility_manager_api_gateway.id
   resource_id      = aws_api_gateway_resource.utility_manager_api_gateway_resource.id
   http_method      = "GET"
-  authorization    = "COGNITO_USER_POOL"
+  authorization    = "COGNITO_USER_POOLS"
   api_key_required = false
   authorizer_id    = aws_api_gateway_authorizer.utility_manager_api_gateway_authorizer.id
 
