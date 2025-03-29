f73201ab6285ac7a3a2b6d647de8703a0b8fb5cb
SaviolaX <withcraft1717@gmail.com>
2025-03-29 16:11:10 +0200

    feat: Update Cognito user pool password policy and username attributes

--- infrastructure/cognito.tf
+++ infrastructure/cognito.tf
@@ -1,13 +1,14 @@
 resource "aws_cognito_user_pool" "utility_manager_user_pool" {
   name = "utility_manager_user_pool"
   password_policy {
-    minimum_length    = 8
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
+    minimum_length    = 6
+    require_lowercase = false
+    require_numbers   = false
+    require_symbols   = false
+    require_uppercase = false
   }
   auto_verified_attributes = ["email"]
+  username_attributes      = ["email"]
 }
 
 resource "aws_cognito_user_pool_client" "utility_manager_user_pool_client" {
@@ -15,5 +16,5 @@ resource "aws_cognito_user_pool_client" "utility_manager_user_pool_client" {
 
   user_pool_id        = aws_cognito_user_pool.utility_manager_user_pool.id
   generate_secret     = false
-  explicit_auth_flows = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  explicit_auth_flows = ["ALLOW_USER_SRP_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
 }
