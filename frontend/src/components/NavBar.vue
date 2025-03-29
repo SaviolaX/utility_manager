f4073b2bdd2000d14789d68fa634362a516b2a2f
SaviolaX <withcraft1717@gmail.com>
2025-03-29 16:10:19 +0200

    feat: Enhance NavBar with user authentication and routing links

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,19 +1,63 @@
 <template>
   <nav class="nav-wrapper">
     <div class="nav-title">
-      <h2>App title</h2>
+      <router-link class="nav-title-link" to="/"
+        ><h2>App title</h2></router-link
+      >
     </div>
     <div class="nav-auth">
-      <p>Current user,</p>
-      <a href="#">Login</a>
-      <a href="#">Register</a>
-      <a href="#">Logout</a>
+      <p v-if="authStore.user">Current user, {{ user.email }}</p>
+      <button v-if="authStore.user" @click="signOutUser">Logout</button>
+      <router-link v-if="!authStore.user" to="/signin">Login</router-link>
+      <router-link v-if="!authStore.user" to="/signup">Register</router-link>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+import { signOut } from "aws-amplify/auth";
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const authStore = useAuthStore();
+    const router = useRouter();
+    const user = computed(() => authStore.user);
+
+    const signOutUser = async () => {
+      try {
+        await signOut();
+        authStore.clearUser();
+        router.push("/signin");
+      } catch (err) {
+        console.error("Sign-out error: ", err);
+      }
+    };
+
+    return { authStore, signOutUser, user };
+  },
+  async mounted() {
+    try {
+      const user = await getCurrentUser();
+      if (user) {
+        this.authStore.setUser(
+          {
+            username: user.username,
+            email: user.signInDetails?.loginId,
+          },
+          {
+            isLoggedIn: true,
+          }
+        );
+        console.log(`User ${user.username} logged in.`);
+      }
+    } catch (err) {
+      console.log("No user on mounted NavBar.");
+    }
+  },
+};
 </script>
 
 <style>
@@ -38,5 +82,9 @@ export default {};
 }
 .nav-auth a:hover {
   text-decoration: underline;
+}
+.nav-title-link {
+  text-decoration: none;
+  color: aliceblue;
 }
 </style>
