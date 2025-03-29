593ea0be34309be15130d8850b3ed67f792081cd
SaviolaX <withcraft1717@gmail.com>
2025-03-29 16:09:47 +0200

    feat: Integrate Pinia for state management and configure AWS Amplify in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
+import { Amplify } from "aws-amplify";
+import awsconfig from "../aws-exports";
 
-createApp(App).mount('#app')
+Amplify.configure(awsconfig);
+
+const app = createApp(App);
+const pinia = createPinia(); // dependency for storing global states
+
+app.use(pinia);
+app.use(router);
+app.mount("#app");
