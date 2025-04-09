1a0664b78694a0bc1079551f2b88b4120ad8d534
SaviolaX <withcraft1717@gmail.com>
2025-04-09 20:27:52 +0300

    feat: Refactor lambda_handler to integrate DynamoDB and streamline utility data handling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,118 +1,102 @@
 import json
 
 import boto3
+from core.db_handler import UtilitiesDB
 from core.models import Utility_manager
+from core.services import response_handler
 
 STATUS_CODE_OK = 200
 ACA_ORIGIN = "*"
 
 
-class DBHanbler:
+db_arn = "arn:aws:dynamodb:eu-central-1:153061395159:table/utility_manager_db"
 
-    def get_utilities(self) -> list[dict]:
-        with open("database.json", "r") as f:
-            data = json.load(f)
-        return data
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(db_arn)
 
-    def add_utilities(self, utility):
-        with open("database.json", "r") as f:
-            data = json.load(f)
-            print("Before adding utility:", len(data))
-        data.append(utility)
-        with open("database.json", "w") as f:
-            json.dump(data, f, indent=4)
-        print("Utility added to database")
-        print("After adding utility:", len(data))
-
-
-def response_handler(status_code: int, body: dict, origin: str) -> dict:
-    return {
-        "statusCode": status_code,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": origin,
-        },
-        "body": json.dumps(body),
-    }
+db = UtilitiesDB(table)
 
 
 def lambda_handler(event: dict, context: dict) -> dict:
 
-    user = (
+    username = (
         event.get("requestContext")
         .get("authorizer")
         .get("claims")
         .get("cognito:username")
     )
-    email = event.get("requestContext").get("authorizer").get("claims").get("email")
 
-    db_handler = DBHanbler()
+    # TODO:
+    # Convert date from single line in frontend
+    # Fix watch method after first item loaded
+    # Update items after post request
+    # Add try/except
+    # To think about initiation db inside lambda or outside
 
     if event.get("httpMethod") == "GET":
+        with open("database.json", "r") as f:
+            data = json.load(f)
 
-        utilities_list = db_handler.get_utilities()
+        resp = db.get_all(user_id=username)
+        print(resp)
 
         return response_handler(
             status_code=STATUS_CODE_OK,
             body={
-                "utilities_list": utilities_list,
+                "utilities_list": resp["Items"],
             },
             origin=ACA_ORIGIN,
         )
 
     if event.get("httpMethod") == "POST":
-        previous_month = db_handler.get_utilities()[-1]
-        payload = json.loads(event.get("body")).get("body").get("payload")
-        utilities = Utility_manager.from_json(payload)
-        result = utilities.calculate_costs(previous_month)
 
-        return response_handler(
-            status_code=STATUS_CODE_OK,
-            body={
-                "result": result,
-            },
-            origin=ACA_ORIGIN,
-        )
+        all_data = db.get_all(user_id=username)
+
+        if all_data["Count"] != 0:
+            # new_data = json.loads(event["body"])["body"]["payload"]
+            # old_data = data[-1]
+
+            # um = Utility_manager.from_json(new_data)
+            # result = um.calculate_costs(old_data)
+
+            # resp = db.add(
+            #     p_key=username,
+            #     s_key=result["date"],
+            #     utilities=result["utilities"],
+            #     prices=result["prices"],
+            #     cost=result["cost"],
+            # )
+
+            return response_handler(
+                status_code=STATUS_CODE_OK,
+                body={
+                    "response": all_data["Count"],
+                },
+                origin=ACA_ORIGIN,
+            )
+        else:
+            new_data = json.loads(event["body"])["body"]["payload"]
+            um = Utility_manager.from_json(new_data)
+            formatted_data = um.format_for_first_record()
+            resp = db.add(
+                p_key=username,
+                s_key=formatted_data["date"],
+                utilities=formatted_data["utilities"],
+                prices=formatted_data["prices"],
+                cost=formatted_data["cost"],
+            )
+            return response_handler(
+                status_code=STATUS_CODE_OK,
+                body={
+                    "message": "First record added.",
+                },
+                origin=ACA_ORIGIN,
+            )
 
 
 # if __name__ == "__main__":
 
-#     body ={
-#       "payload":{
-#         "date": "2023-08-01",
-#         "utilities":{
-#             "gas":90,
-#             "electricity":{
-#                "t1":180,
-#                "t2":150
-#             },
-#             "water":{
-#                "cold":{
-#                   "kitchen":40,
-#                   "bathroom":35,
-#                },
-#                "hot":{
-#                   "kitchen":40,
-#                   "bathroom":50
-#                }
-#             }
-#          },
-#          "prices":{
-#             "gas": 0.5,
-#             "gas_distribution": 0.2,
-#             "electricity":{
-#                "t1":0.4,
-#                "t2":0.3
-#             },
-#             "water":{
-#                "cold":0.2,
-#                "hot":0.3
-#             }
-#          }
-#       }
-#    }
-
-#     event = {
+#     post_event = {
 #         "httpMethod": "POST",
 #         "requestContext": {
 #             "authorizer": {
@@ -122,7 +106,21 @@ def lambda_handler(event: dict, context: dict) -> dict:
 #                 }
 #             }
 #         },
-#         "body": json.dumps(body),
+#         "body": '{"body":{"payload":{"utilities":{"gas":123,"electricity":{"t1":222,"t2":222},"water":{"cold":{"kitchen":123,"bathroom":123},"hot":{"kitchen":123,"bathroom":123}}},"prices":{"gas":0.5,"gas_distribution":0.1,"electricity":{"t1":0.4,"t2":0.3},"water":{"cold":0.2,"hot":0.3}}}}}',
+#     }
+
+#     get_event = {
+#         "httpMethod": "GET",
+#         "requestContext": {
+#             "authorizer": {
+#                 "claims": {
+#                     "cognito:username": "test_user",
+#                     "email": "test@mail.com",
+#                 }
+#             }
+#         },
+#         # "body": json.dumps(),
 #     }
 #     context = {}
-#     response = lambda_handler(event, context)
+#     # response = lambda_handler(get_event, context)
+#     response = lambda_handler(post_event, context)
