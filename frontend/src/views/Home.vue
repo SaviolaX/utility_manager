f857869d794be0d50df9603fde4eb4eeb13eead3
SaviolaX <withcraft1717@gmail.com>
2025-04-11 21:20:48 +0300

    feat: Add dateToString method for formatted date string generation

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -354,6 +354,17 @@ export default {
     }
   },
   methods: {
+    dateToString() {
+      const date = new Date();
+      let year = date.getFullYear();
+      let month = String(date.getMonth() + 1).padStart(2, "0");
+      let day = String(date.getDate()).padStart(2, "0");
+      let hour = String(date.getHours()).padStart(2, "0");
+      let minute = String(date.getMinutes()).padStart(2, "0");
+      let second = String(date.getSeconds()).padStart(2, "0");
+      let millisecond = String(date.getMilliseconds()).padStart(2, "0");
+      return `${year}${month}${day}${hour}${minute}${second}${millisecond}`;
+    },
     sortByDate(dataLst, order = "asc") {
       const sorted = [...dataLst]; // Avoid mutating the original array
       return order === "desc"
